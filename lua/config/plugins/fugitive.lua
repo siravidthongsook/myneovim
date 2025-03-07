4e73ba7129cb6e6d74eb19654ccb13ad02c3eab5
Siravid Thongsook <ppoumppoum@gmail.com>
2025-03-07 17:35:11 +0700

    Add git push hotkey

--- lua/config/plugins/fugitive.lua
+++ lua/config/plugins/fugitive.lua
@@ -4,6 +4,7 @@ return {
         vim.keymap.set("n", "<leader>gc", "<CMD>Git commit -a<CR>")
         vim.keymap.set("n", "<leader>gg", "<CMD>Git<CR>")
         vim.keymap.set("n", "<leader>ga", "<CMD>Git add .<CR>")
+        vim.keymap.set("n", "<leader>gp", ":Git push")
     end
 
 }
