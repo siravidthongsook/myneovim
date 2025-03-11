7f6093372899cdfd931b8efe7a8eeda9b61c8f6d
Siravid Thongsook <ppoumppoum@gmail.com>
2025-03-11 22:08:42 +0700

    install new theme

--- lua/config/plugins/theme.lua
+++ lua/config/plugins/theme.lua
@@ -1,6 +1,14 @@
 return {
-	'rebelot/kanagawa.nvim',
-	config = function()
-		vim.cmd("colorscheme kanagawa")
-	end
+    {
+        'rebelot/kanagawa.nvim',
+        config = function()
+            -- vim.cmd("colorscheme kanagawa")
+        end
+    },
+    {
+        'Mofiqul/vscode.nvim',
+        config = function()
+            vim.cmd("colorscheme vscode")
+        end
+    },
 }
